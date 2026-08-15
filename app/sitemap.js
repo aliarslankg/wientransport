de9c9ec67dbe36b7bc62edf82a77f503f112ba76
aliarslankg <aa66tx@gmail.com>
2026-08-15 23:25:20 +0200

    Add transport-wien to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 export default function sitemap(){
  const base="https://wientransport.at";
- const paths=["/", "/en", "/tr", "/ru", "/ar", "/umzug-wien", "/moebeltransport-wien", "/kleintransport-wien", "/moebelmontage-wien", "/firmenumzug-wien", "/entruempelung-wien", "/kurierdienst-wien", "/pakettransport-europa","/ueber-uns","/impressum","/datenschutz"];
+ const paths=["/", "/en", "/tr", "/ru", "/ar", "/umzug-wien","/transport-wien", "/moebeltransport-wien", "/kleintransport-wien", "/moebelmontage-wien", "/firmenumzug-wien", "/entruempelung-wien", "/kurierdienst-wien", "/pakettransport-europa","/ueber-uns","/impressum","/datenschutz"];
  return paths.map((p)=>({url:`${base}${p}`,lastModified:new Date(),changeFrequency:"weekly",priority:p==="/" ? 1 : .85}));
 }
